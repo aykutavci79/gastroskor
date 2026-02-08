52f97e4ecda9db9d97b0109ed4b72ffa82b175a7
Aykut <aykut.avci@hotmail.com>
2026-02-08 21:30:48 +0300

    fix utf8 encoding for route

--- app/admin/account/password/route.ts
+++ app/admin/account/password/route.ts
@@ -1,4 +1,4 @@
-﻿import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import bcrypt from "bcryptjs";
 import { authOptions } from "@/lib/auth-options";
